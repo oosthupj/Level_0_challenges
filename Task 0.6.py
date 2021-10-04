621a40342e6ec055c4b472d77c8c3f6b92bc457b
oosthupj <91240785+oosthupj@users.noreply.github.com>
2021-10-04 12:12:46 +0200

    Add files via upload

--- Task 0.6.py
+++ Task 0.6.py
@@ -1,13 +1,29 @@
 
 def get_maximum(a,b,c):
-    m=0
-    if a>b:
-        m=a
-    if b>a:
-        m=b
-    if c>m:
-        m=c
-    print(m)    
+    
+    a_is_bigger=False
+    b_is_bigger=False
+    c_is_bigger=False
+    if a>=b:
+        a_is_bigger=True 
+    if b>a: 
+        b_is_bigger= True
+    
+    if a_is_bigger and c>=a:
+        c_is_bigger=True
+        a_is_bigger = False
+    if b_is_bigger and c>=b:
+        c_is_bigger=True
+        b_is_bigger=False
 
+    if a_is_bigger:
+        print(a)
+    elif b_is_bigger:
+        print(b)
+    elif c_is_bigger:
+        print(c)
+ 
 
 get_maximum(1,2,9)
+get_maximum(1,1,0)
+get_maximum(-1,-1,-2)
